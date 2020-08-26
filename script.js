9c44dd20cb13a321d61117ba9e09d7acab920338
juanHernandez <the.correo@protonmail.com>
2020-08-26 13:02:42 -0500

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("hello from js")
 
-counter = 42
+counter = 1
 url_base = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"
 extension = ".svg"
 
 
 url = url_base + counter + extension
-
 pokemon = document.getElementById('pokemon')
+pokemon.src = url
 
 back_button = document.getElementById('back-button')
 
@@ -17,7 +17,7 @@ back_button.onclick = function(){
     counter = 151
 
   url = url_base + counter + extension
-  //console.log(url)
+  console.log(counter)
   pokemon.src = url
 }
 
@@ -29,6 +29,6 @@ forward_button.onclick = function(){
     counter = 1
 
   url = url_base + counter + extension
-  //console.log(url)
+  console.log(counter)
   pokemon.src = url
 }
